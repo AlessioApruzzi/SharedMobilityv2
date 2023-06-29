74c88f6ae983dcc04706d4300bd18b2cfdba6f4f
AlessioApruzzi <134047416+AlessioApruzzi@users.noreply.github.com>
2023-06-29 16:50:29 +0200

    Repo init

---
+++ src/main/java/com/treeschool/sharedmobility/sharedmobility/SharedmobilityApplication.java
@@ -0,0 +1,13 @@
+package com.treeschool.sharedmobility.sharedmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedmobilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedmobilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treeschool/sharedmobility/sharedmobility/SharedmobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treeschool.sharedmobility.sharedmobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedmobilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
